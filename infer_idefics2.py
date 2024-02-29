adabcb074c227a86cd7d566d1a2b5556d17aaca6
VictorSanh <victorsanh@gmail.com>
2024-02-29 22:45:25 +0000

    proper quantization and fused quantized

--- infer_idefics2.py
+++ infer_idefics2.py
@@ -1,22 +1,43 @@
 import requests
 import torch
 from PIL import Image
+from io import BytesIO
+import time
 
-from awq import AutoAWQForCausalLM
-from transformers import AutoProcessor
+from transformers import AutoProcessor, AutoModelForCausalLM, AwqConfig
 from transformers.image_utils import to_numpy_array, PILImageResampling, ChannelDimension
 from transformers.image_transforms import resize, to_channel_dimension_format
 
 
-quant_path = "/admin/home/victor/code/idefics2-awq"
+MODE = "quantized"
+DEVICE = "cuda:0"
 
 # Load model
-model = AutoAWQForCausalLM.from_quantized(quant_path, safetensors=True, device_map={"": 0})
-processor = AutoProcessor.from_pretrained("HuggingFaceM4/idefics2")
+if MODE == "regular":
+    model = AutoModelForCausalLM.from_pretrained("HuggingFaceM4/idefics2", torch_dtype=torch.float16, trust_remote_code=True).to(DEVICE)
+elif MODE == "quantized":
+    quant_path = "/admin/home/victor/code/idefics2-awq"
+    model = AutoModelForCausalLM.from_pretrained(quant_path, trust_remote_code=True).to(DEVICE)
+elif MODE == "fused_quantized":
+    quant_path = "/admin/home/victor/code/idefics2-awq"
+    quantization_config = AwqConfig(
+        bits=4,
+        fuse_max_seq_len=4096,
+        modules_to_fuse={
+            "attention": ["q_proj", "k_proj", "v_proj", "o_proj"],
+            "mlp": ["gate_proj", "up_proj", "down_proj"],
+            "layernorm": ["input_layernorm", "post_attention_layernorm", "norm"],
+            "use_alibi": False,
+            "num_attention_heads": 32,
+            "num_key_value_heads": 8,
+            "hidden_size": 4096,
+        }
+    )
+    model = AutoModelForCausalLM.from_pretrained(quant_path, quantization_config=quantization_config, trust_remote_code=True).to(DEVICE)
+else:
+    raise ValueError("Unknown mode")
 
-prompt = "USER: <image>\nWhat are these?\nASSISTANT:"
-image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
-raw_image = Image.open(requests.get(image_file, stream=True).raw)
+processor = AutoProcessor.from_pretrained("HuggingFaceM4/idefics2")
 
 image_seq_len = model.config.perceiver_config.resampler_n_latents
 BOS_TOKEN = processor.tokenizer.bos_token
@@ -64,22 +85,69 @@ def custom_transform(x):
     return x
 
 
+def download_image(url):
+    try:
+        # Send a GET request to the URL to download the image
+        response = requests.get(url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Open the image using PIL
+            image = Image.open(BytesIO(response.content))
+            # Return the PIL image object
+            return image
+        else:
+            print(f"Failed to download image. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+# Create text token inputs
+image_seq = '<image>' * image_seq_len
 inputs = processor.tokenizer(
-    f"{BOS_TOKEN}<fake_token_around_image>{'<image>' * image_seq_len}<fake_token_around_image>In this image, we see",
+    [
+        f"{BOS_TOKEN}<fake_token_around_image>{image_seq}<fake_token_around_image>In this image, we see",
+        f"{BOS_TOKEN}bla bla<fake_token_around_image>{image_seq}<fake_token_around_image>{image_seq}<fake_token_around_image>",
+    ],
     return_tensors="pt",
     add_special_tokens=False,
-)
-inputs["pixel_values"] = processor.image_processor([raw_image], transform=custom_transform)
-inputs = {
-    k: v.to(0, torch.float16) if k != "input_ids" else v.to(0)
-    for k, v in inputs.items()
-}
-
-# Generate output
-generation_output = model.generate(
-    **inputs,
-    max_new_tokens=512,
-    bad_words_ids=BAD_WORDS_IDS
+    padding=True,
 )
 
-print(processor.decode(generation_output[0], skip_special_tokens=True))
+# Create pixel inputs
+image1 = download_image("https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg")
+image2 = download_image("https://cdn.britannica.com/59/94459-050-DBA42467/Skyline-Chicago.jpg")
+image3 = download_image("https://thumbs.dreamstime.com/b/golden-gate-bridge-san-francisco-purple-flowers-california-echium-candicans-36805947.jpg")
+raw_images = [
+    [image1],
+    [image2, image3],
+]
+output_images = [
+    [processor.image_processor(img, transform=custom_transform) for img in img_list]
+    for img_list in raw_images
+]
+total_batch_size = len(output_images)
+max_num_images = max([len(img_l) for img_l in output_images])
+max_height = max([i.size(2) for img_l in output_images for i in img_l])
+max_width = max([i.size(3) for img_l in output_images for i in img_l])
+padded_image_tensor = torch.zeros(total_batch_size, max_num_images, 3, max_height, max_width)
+padded_pixel_attention_masks = torch.zeros(
+    total_batch_size, max_num_images, max_height, max_width, dtype=torch.bool
+)
+for batch_idx, img_l in enumerate(output_images):
+    for img_idx, img in enumerate(img_l):
+        im_height, im_width = img.size()[2:]
+        padded_image_tensor[batch_idx, img_idx, :, :im_height, :im_width] = img
+        padded_pixel_attention_masks[batch_idx, img_idx, :im_height, :im_width] = True
+
+inputs["pixel_values"] = padded_image_tensor
+inputs["pixel_attention_mask"] = padded_pixel_attention_masks
+inputs = {k: v.to(DEVICE) for k, v in inputs.items()}
+
+start = time.time()
+generated_ids = model.generate(**inputs, bad_words_ids=BAD_WORDS_IDS, max_new_tokens=500)
+generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+
+print(generated_texts)
+print(time.time() - start)
--- quantize_idefics2.py
+++ quantize_idefics2.py
@@ -1,6 +1,6 @@
 import torch
 from awq import AutoAWQForCausalLM
-from transformers import AutoTokenizer, AutoProcessor
+from transformers import AutoTokenizer, AutoProcessor, AwqConfig
 
 model_path = "HuggingFaceM4/idefics2"
 quant_path = "/admin/home/victor/code/idefics2-awq"
@@ -16,6 +16,16 @@ processor = AutoProcessor.from_pretrained(model_path)
 
 # Quantize
 model.quantize(tokenizer, quant_config=quant_config)
+
+# Modify config file
+quantization_config = AwqConfig(
+    bits=quant_config["w_bit"],
+    group_size=quant_config["q_group_size"],
+    zero_point=quant_config["zero_point"],
+    version=quant_config["version"].lower(),
+    modules_to_not_convert=["model.vision_model", "model.modality_projection", "model.perceiver_resampler"],
+).to_dict()
+model.model.config.quantization_config = quantization_config
 
 # Save quantized model
 model.save_quantized(quant_path)
